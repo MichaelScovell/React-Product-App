122d91e265e939c32a1f18a8df8fe0ae799c3bad
MichaelScovell <michael.scovell@cevo.com.au>
2023-06-10 18:37:21 +1000

    Implemented the Homepage

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useSnapshot } from "valtio";
+// import state
+import state from "../store";
+
+const CustomButton = ({ type, title, handleClick, customStyles }) => {
+  // Defining state
+  const snap = useSnapshot(state);
+  // Defining a function for generateStyle
+  const generateStyle = (type) => {
+    // Check if the button is of the correct type
+    if (type === "filled") {
+      return {
+        // Set background colors
+        backgroundColor: snap.color,
+        color: "#fff",
+      };
+    }
+  };
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
