74a8ec7c2c368e29a4b12b04ef88964b7e9903ef
MichaelScovell <michael.scovell@cevo.com.au>
2023-06-13 12:41:14 +1000

    Added Shadows

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -4,10 +4,15 @@ import { useFrame } from '@react-three/fiber'
 import { AccumulativeShadows, RandomizedLight } from '@react-three/drei'
 
 const Backdrop = () => {
+  // Define a reference variable for shadows
+  const shadows = useRef();
   return (
     // Backdrop Component for displaying shadows and lighting
-    <AccumulativeShadows position={[0, 0, -0.14]}>
-      <RandomizedLight amount={4}/>
+    <AccumulativeShadows ref={shadows} temporal frames={60} alphaTest={0.85} scale={10} rotation={[Math.PI / 2, 0, 0]} position={[0, 0, -0.14]}>
+      {/* Shadow from Left */}
+      <RandomizedLight amount={4} radius={9} intensity={0.55} ambient={0.25} position={[5, 5, -10]}/>
+      {/* Shadow from right */}
+      <RandomizedLight amount={4} radius={5} intensity={0.25} ambient={0.55} position={[-5, 5, -9]}/>
     </AccumulativeShadows>
   )
 }
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -8,6 +8,7 @@ import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
   return (
+    // Canvas of the rendered image
     <Canvas shadows camera={{position: [0, 0, 0], fov: 25}} gl={{preserveDrawingBuffer: true}} className='w-full max-w-full transition-all ease-in'>
       <ambientLight intensity={0.5}/>
       <Environment preset='city'/>
